b13289ddfc816ee8e2a5f98d57ea6001767e8090
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-16 14:01:03 +0100

    adds map component

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="map">
+        <h4>i am the map component</h4>
+      </div>
+    );
+  }
+}
+
+export default Map;
