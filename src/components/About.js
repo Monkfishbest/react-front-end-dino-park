fdf4ae43039b85d1723c927f694ae7a58401048b
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-16 13:38:14 +0100

    adds About component in components

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="about">
+        <h4>i am the about component</h4>
+      </div>
+    );
+  }
+}
+
+export default About;
