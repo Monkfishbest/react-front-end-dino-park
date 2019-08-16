a394ce8d5b410e73dc5fcbf4035c034181535afa
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-16 15:43:55 +0100

    adds dinosaur list component

---
+++ src/components/DinosaurList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class DinosaurList extends Component {
+  render() {
+    return (
+      <div className="dinosaur-list">
+        <h4>Dinosaur List:</h4>
+          <ul>
+            <li>dinosaur</li>
+            <li>dinosaur</li>
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default DinosaurList;
