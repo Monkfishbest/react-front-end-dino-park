1d178690b0c8db24be56b011572c41e87889aceb
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-16 15:31:56 +0100

    adds Paddock component

---
+++ src/components/Paddock.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Paddock extends Component {
+  render() {
+    return (
+      <div className="paddock">
+        <h4>i am the paddock component</h4>
+      </div>
+    );
+  }
+}
+
+export default Paddock;
