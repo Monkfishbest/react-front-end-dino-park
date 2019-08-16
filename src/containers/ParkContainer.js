bc733b946c026b941aedbc4b6e4f614074d66de3
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-16 13:28:43 +0100

    added containers folder, ParkContainer, and made ParkContainer show in app.js

---
+++ src/containers/ParkContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ParkContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="park-container">
+        <h2>This is a park container</h2>
+      </div>
+    );
+  }
+}
+
+export default ParkContainer;
