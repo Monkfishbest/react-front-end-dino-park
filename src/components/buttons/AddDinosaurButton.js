c09b3616218d11d9a8f0b655a885922c89c84398
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-18 12:12:25 +0100

    adds AddDinosaurButton to button list

---
+++ src/components/buttons/AddDinosaurButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddDinosaurButton extends Component {
+  render() {
+    return (
+      <div className="add-dinosaur-button">
+        <button type="button" value="add-dinosaur">Add A Dinosaur</button>
+      </div>
+    );
+  }
+}
+
+export default AddDinosaurButton;
--- src/components/buttons/ButtonList.js
+++ src/components/buttons/ButtonList.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import TransferButton from "./TransferButton"
+import AddDinosaurButton from "./AddDinosaurButton"
 
 class ButtonList extends Component {
   render() {
     return (
       <div className="button-list">
         <TransferButton/>
+        <AddDinosaurButton/>
       </div>
     );
   }
