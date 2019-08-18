29edd108080a282f72e9be38b938f7f810e34e0b
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-08-18 12:09:16 +0100

    added router links, some bugs most likey syntatic in nature going to fix now

---
+++ src/components/PaddockList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class PaddockList extends Component {
+  render() {
+    return (
+      <div className="paddock-list">
+        <h4>Paddock List:</h4>
+          <ul>
+            <li>paddock1</li>
+            <li>paddock2</li>
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default PaddockList;
--- src/containers/ParkContainer.js
+++ src/containers/ParkContainer.js
@@ -1,8 +1,10 @@
-import NavBar from "../components/NavBar";
-import Home from "../components/Home";
-import React, { Component } from "react";
-import Map from '../components/Map.js';
+import NavBar from '../components/NavBar';
+import Home from '../components/Home';
+import Map from '../components/Map';
+import Faq from '../components/FAQ';
+import About from '...components/About';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import React, { Component } from "react";
 
 class ParkContainer extends Component {
 
@@ -20,8 +22,10 @@ class ParkContainer extends Component {
         <React.Fragment>
         <NavBar/>
         <Switch>
-
         <Route path="/home" component={Home}/>
+        <Route path="/map" component={Map}/>
+        <Route path="/About" component={About}/>
+        <Route path="/FAQ" component={Faq}/>
         </Switch>
         </React.Fragment>
       </Router>
