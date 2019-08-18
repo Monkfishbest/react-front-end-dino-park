a0b689cf961643eace7bee0aa600bf2bbf8b584a
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-08-18 15:25:08 +0100

    added the fetch request, the contructor and passed down the list of paddocks as prop to paddockList.js

--- src/containers/PaddockContainer.js
+++ src/containers/PaddockContainer.js
@@ -3,13 +3,32 @@ import PaddockList from '../components/map/PaddockList';
 import ButtonList from '../components/buttons/ButtonList';
 
 class PaddockContainer extends Component {
+
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        listofPaddocks: []
+      };
+    }
+
+  componentDidMount(){
+    const url = 'PLACEHOLDER-PLACEHOLDER-PLACEHOLDER'
+    fetch(url)
+    .then(res => res.json())
+    .then(returnedDinos => this.setState({listOfDinos: returnedDinos, listOfPaddocks: returnedDinos}))
+    .catch(err => console.error(err))
+  }
+
+
   render(){
     return (
       <div className="PaddockContainer">
-      <PaddockList/>
+      <PaddockList paddockList={this.state.listOfPaddocks}/>
       <ButtonList/>
       </div>
     )
+
   }
 }
 
--- src/containers/ParkContainer.js
+++ src/containers/ParkContainer.js
@@ -12,7 +12,7 @@ class ParkContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listOfDinos: []
+      listOfDinos: []g
     };
   }
 
