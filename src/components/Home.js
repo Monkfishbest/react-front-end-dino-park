69ead317c3f63dd6dccc63daa103a785dac578fc
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-19 11:31:42 +0100

    removed form from home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import AddDinosaurForm from "./forms/AddDinosaurForm"
 
 const Home = () => (
   <div>
     <h4>Home</h4>
     <p>Hello and welcome to DinoPark mangagment system.</p>
-    <AddDinosaurForm/>
   </div>
 )
 
