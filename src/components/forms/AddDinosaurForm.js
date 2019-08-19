11a79b66fd11f8e44312a2437aa5be4c2e9fd742
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-19 13:44:57 +0100

    add dinosaur form updates state

--- src/components/forms/AddDinosaurForm.js
+++ src/components/forms/AddDinosaurForm.js
@@ -2,23 +2,70 @@ import React, { Component } from "react";
 
 class AddDinosaurForm extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      isHerbivore: null,
+      fullnessLevel: null,
+      type: null,
+      paddock: null
+
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleIsHerbivoreChange = this.handleIsHerbivoreChange.bind(this);
+    this.handleFullnessLevelChange = this.handleFullnessLevelChange.bind(this);
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+    this.handlePaddockChange = this.handlePaddockChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event){
+    this.setState({ name: event.target.value})
+  }
+
+  handleIsHerbivoreChange(event){
+    this.setState({isHerbivore: event.target.value})
+  }
+
+  handleFullnessLevelChange(event){
+    this.setState({fullnessLevel: event.target.value})
+  }
+
+  handleTypeChange(event){
+    this.setState({type: event.target.value})
+  }
+
+  handlePaddockChange(event){
+    this.setState({paddock: event.target.value})
+  }
+
+  handleSubmit(event) {
+    const newDino = this.state;
+    fetch('/api/form-submit-url', {
+      method: 'POST',
+      body: newDino,
+    });
+    this.setState({name: "", isHerbivore: null, fullnessLevel: null, type: null, paddock: null})
+  }
+
   render() {
     return (
       <div className="add-dinosaur-form">
         <h2>Add A Dinosaur:</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <label>Name:</label>
-            <input type="text"/>
+            <input type="text" onChange={this.handleNameChange} value={this.state.name}/>
           <br/>
             <label>Diet:</label>
-            <input id="herbivore" type="radio" name="diet" value="true" />Herbivore
-            <input id="carnivore" type="radio" name="diet" value="false"/>Carnivore
+            <input id="herbivore" type="radio" name="diet" value={true} onChange={this.handleIsHerbivoreChange}/>Herbivore
+            <input id="carnivore" type="radio" name="diet" value={false} onChange={this.handleIsHerbivoreChange}/>Carnivore
           <br/>
             <label>Fullness Level:</label>
-            <input type="number" min="0" max="100"/>
+            <input type="number" min="0" max="100" onChange={this.handleFullnessLevelChange} value={this.state.fullnessLevel}/>
           <br/>
             <label>Species:</label>
-            <select name="type">
+            <select name="type" onChange={this.handleTypeChange} value={this.state.type}>
                 <option selected disabled>choose type</option>
                 <option value="mososaurus" id="carnivores">Mososaurus</option>
                 <option value="velociraptor" id="carnivores">Velociraptor</option>
@@ -34,7 +81,7 @@ class AddDinosaurForm extends Component {
             </select>
           <br/>
             <label>Paddock:</label>
-            <select name="type">
+            <select name="type" onChange={this.handlePaddockChange} value={this.state.paddock}>
                 <option selected disabled>choose type</option>
                 <option value="paddock1">Paddock1</option>
                 <option value="paddock2">Paddock2</option>
