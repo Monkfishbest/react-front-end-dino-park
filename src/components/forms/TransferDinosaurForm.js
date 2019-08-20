20d6c9f20a0748ed2560bfcb5d4676880233df1b
Robert Tam <rsh.tam@gmail.com>
2019-08-20 23:58:12 +0100

    refactor dropdown to display list of herbivores and paddocks

--- src/components/forms/TransferDinosaurForm.js
+++ src/components/forms/TransferDinosaurForm.js
@@ -10,6 +10,20 @@ class TransferDinosaurForm extends Component {
     this.handleDinosaurChange = this.handleDinosaurChange.bind(this);
     this.handlePaddockChange = this.handlePaddockChange.bind(this);
     this.handleTransferSubmit = this.handleTransferSubmit.bind(this);
+    this.getHerbivoreList = this.getHerbivoreList.bind(this);
+    this.getPaddockList = this.getPaddockList.bind(this);
+  }
+
+  getHerbivoreList() {
+    return this.props.herbivoreList.map(herbivore => {
+      return(<option>{herbivore.name}</option>)
+    });
+  }
+
+  getPaddockList() {
+    return this.props.paddockList.map(paddock => {
+      return(<option>{paddock.name}</option>)
+    });
   }
 
   handleDinosaurChange(event) {
@@ -33,15 +47,13 @@ class TransferDinosaurForm extends Component {
       <div>
         <form className="transfer-dino-form">
           <select>
-            <option disabled value="default">Select a Dinosaur</option>
-            <option>Dino1</option>
-            <option>Dino2</option>
+            <option selected disabled value="default">Select a Dinosaur</option>
+            {this.getHerbivoreList()}
           </select>
 
           <select>
-          <option disabled value="default">Select a Paddock</option>
-          <option>Paddock1</option>
-          <option>Paddock2</option>
+            <option selected disabled value="default">Select a Paddock</option>
+            {this.getPaddockList()}
           </select>
 
           <input className="button" type="submit" value="Transfer Dinosaur" />
