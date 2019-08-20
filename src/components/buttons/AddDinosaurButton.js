2dff92ce80a48ba51fcc13dba8867c7f0f25f7cb
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-08-20 11:11:36 +0100

    refactored so the logic is in the addDinoButton componenet to try and get it to work, can refactor later

--- src/components/buttons/AddDinosaurButton.js
+++ src/components/buttons/AddDinosaurButton.js
@@ -3,10 +3,23 @@ import './Buttons.css';
 import AddDinosaurForm from '../forms/AddDinosaurForm'
 
 class AddDinosaurButton extends Component {
+  constructor(props){
+    super(props);
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.state = {
+      isShowForm: false
+    };
+  }
+
+  handleButtonClick(){
+    this.setState({isShowForm: !this.state.isShowForm})
+  }
+
   render() {
+
     return (
       <div className="add-dinosaur-button">
-        <button type="button" value="add-dinosaur">Add A Dinosaur</button>
+        <button type="button" value="add-dinosaur" onClick={this.handleButtonClick}>Add A Dinosaur</button>
         <AddDinosaurForm onAddDinosaurFormSubmit={this.props.onAddDinosaurFormSubmit}/>
       </div>
     );
--- src/components/buttons/ShowFormControll.js
+++ src/components/buttons/ShowFormControll.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-
-class ShowFormControll extends Component {
-  constructor(props){
-    super(props);
-    this.handleShowFormClick = this.handleShowFormClick.bind(this);
-    this.handleHideFormClick = this.handleHideFormClick.bind(this);
-    this.state = {
-      isShowForm: false
-    };
-  }
-
-  handleShowFormClick(){
-    this.setState({isShowForm:true})
-  }
-
-  handleHideFormClick(){
-    this.setState({isShowForm:false})
-  }
-
-  render() {
-    const isShowForm = this.state.isShowForm;
-    let button;
-
-    if (isShowForm) {
-      button = <AddDinosaurButton onClick={this.handleHideFormClick}/>
-    } else {
-      button = <AddDinosaurButton onClick={this.handleShowFormClick}/>
-    }
-    return (
-      <div>
-      <AddDinosaurButton isShowForm={isShowForm}/>
-      </div>
-    )
-  }
-}
+// import React, { Component } from 'react';
+// import AddDinosaurButton from './AddDinosaurButton';
+//
+// class ShowFormControll extends Component {
+//   constructor(props){
+//     super(props);
+//     this.handleShowFormClick = this.handleShowFormClick.bind(this);
+//     this.handleHideFormClick = this.handleHideFormClick.bind(this);
+//     this.state = {
+//       isShowForm: false
+//     };
+//   }
+//
+//   handleShowFormClick(){
+//     this.setState({isShowForm:true})
+//   }
+//
+//   handleHideFormClick(){
+//     this.setState({isShowForm:false})
+//   }
+//
+//   render() {
+//     const isShowForm = this.state.isShowForm;
+//     let button;
+//
+//     if (isShowForm) {
+//       button = <AddDinosaurButton onClick={this.handleHideFormClick}/>
+//     } else {
+//       button = <AddDinosaurButton onClick={this.handleShowFormClick}/>
+//     }
+//     return (
+//       <div>
+//         <AddDinosaurButton
+//       </div>
+//     )
+//   }
+// }
+//
+// export default ShowFormControll;
