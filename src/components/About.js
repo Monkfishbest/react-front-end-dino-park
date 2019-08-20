260a5889303f92adda3d9ae577b3a308a38e03f5
Steph Scully <stephanielarascullion@yahoo.com>
2019-08-20 12:48:58 +0100

    Added details and images to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
+import './About.css';
 
 class About extends Component {
   render() {
     return (
       <div className="about">
-        <h4> A group project for CodeClan</h4>
-        <p> Add names/githubs/contact info here?</p> 
+        <h2>Final Group Project at CodeClan</h2>
+        <h3>Upping the ante before graduation!</h3>
+        <p>Our team of 5 chose to create a Dinosaur Park Management System (!) to consolidate our learning of Java, Spring, Javascript and React</p>
+
+        <div className="about-card">
+          <h4>Chae Nicholson</h4>
+          <img alt="Chae Nicholson" src={require('./images/Chae.jpg')}/>
+          <br/>
+          <a href="https://github.com/chaenich">GitHub</a>
+        </div>
+
+        <div className="about-card">
+          <h4>John Patrick McNeill</h4>
+          <img alt="John McNeill" src={require('./images/John.jpg')}/>
+          <br/>
+          <a href="https://github.com/Monkfishbest">GitHub</a>
+        </div>
+
+        <div className="about-card">
+          <h4>Katherine Kerr</h4>
+          <img alt="Katherine Kerr" src={require('./images/Katherine.jpg')}/>
+          <br/>
+          <a href="https://github.com/KatherineLKerr">GitHub</a>
+        </div>
+
+        <div className="about-card">
+          <h4>Stephanie Scullion</h4>
+          <img alt="Steph Scullion" src={require('./images/Steph.jpg')}/>
+          <br/>
+          <a href="https://github.com/StephanieLaraScully">GitHub</a>
+        </div>
+
+        <div className="about-card">
+          <h4>Robert Tam</h4>
+          <img alt="Robert Tam" src={require('./images/Robert.jpg')}/>
+          <br/>
+          <a href="https://github.com/gadgetguy82">GitHub</a>
+        </div>
+
       </div>
     );
   }
