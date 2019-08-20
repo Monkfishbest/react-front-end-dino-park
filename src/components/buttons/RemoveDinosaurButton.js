7335e224600b5d8c021b128fce429862920576c2
Robert Tam <rsh.tam@gmail.com>
2019-08-20 22:04:58 +0100

    Create remove dinosaur button class

---
+++ src/components/buttons/RemoveDinosaurButton.js
@@ -0,0 +1,13 @@
+import React, {} from 'react';
+
+class RemoveDinosaurButton extends Component {
+  render() {
+    return(
+      <div className="remove-dinosaur-button">
+        <button>Remove a Dinosaur</button>
+      </div>
+    )
+  }
+}
+
+export default RemoveDinosaurButton
