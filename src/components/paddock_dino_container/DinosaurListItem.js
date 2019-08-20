c393b43a42e72e16392a15f9f896a5462a42fa74
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-20 09:34:32 +0100

    displays name, type and fullness level

--- src/components/paddock_dino_container/DinosaurListItem.js
+++ src/components/paddock_dino_container/DinosaurListItem.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class DinosaurListItem extends Component {
   render() {
     return (
-        <li>dinosaurlistitem</li>
+        <li>
+        <h3>{this.props.dino.name}</h3>
+        <p>Species: {this.props.dino.type}</p>
+        <p>Hunger Level: {this.props.dino.fullnessLevel}</p>
+        </li>
     );
   }
 }
