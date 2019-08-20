daf690ff8492840fbd77ea4d1e9d640c1531c7f0
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-20 10:15:12 +0100

    form sends correct data to container

--- src/components/paddock_dino_container/DinosaurListItem.js
+++ src/components/paddock_dino_container/DinosaurListItem.js
@@ -7,6 +7,8 @@ class DinosaurListItem extends Component {
         <h3>{this.props.dino.name}</h3>
         <p>Species: {this.props.dino.type}</p>
         <p>Hunger Level: {this.props.dino.fullnessLevel}</p>
+        <p>Paddock: {this.props.dino.paddock.name}</p>
+        <p>Diet: {this.props.dino.herbivore ? "Herbivore" : "Carnivore"}</p>
         </li>
     );
   }
