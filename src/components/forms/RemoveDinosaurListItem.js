249bc280e6f24c73874b2260d376454cf7af0557
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-21 10:24:06 +0100

    Implemented state transfer callback passdown

--- src/components/forms/RemoveDinosaurListItem.js
+++ src/components/forms/RemoveDinosaurListItem.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 
-const RemoveDinosaurListItem = (props) => {
-  return (
-    <li className="remove-dinosaur-list-item">
-      <p> {props.dinosaur.name} </p>
-    </li>
-  );
+class RemoveDinosaurListItem extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dinoToRemove: ''
+    }
+    this.handleRemoveClick = this.handleRemoveClick.bind(this);
+  }
+
+  handleRemoveClick(event) {
+    event.preventDefault();
+    this.props.onRemoveDinoClick({
+      dinoToRemove: 2
+    });
+  }
+
+  render() {
+    return (
+      <li className="remove-dinosaur-list-item">
+        <p> {this.props.dinosaur.name} {this.props.dinosaur.paddock.name} {this.props.dinosaur.type} {this.props.dinosaur.herbivore ? "Herbivore" : "Carnivore"} {this.props.dinosaur.id}</p>
+        <button onClick={this.handleRemoveClick} value={2}></button>
+      </li>
+    );
+  }
 }
 
 export default RemoveDinosaurListItem;
