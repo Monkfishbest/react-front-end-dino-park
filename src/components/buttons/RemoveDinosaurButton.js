5fefcc3d6342c116fa8d36ecfb1d946615e231d5
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-21 10:23:22 +0100

    Implemented state transfer callback passdown

--- src/components/buttons/RemoveDinosaurButton.js
+++ src/components/buttons/RemoveDinosaurButton.js
@@ -29,7 +29,8 @@ class RemoveDinosaurButton extends Component {
       <div className="remove-dinosaur-button">
         <button className="remove-dinosaur-btn" type="button" value="remove-dinosaur" onClick={() => this.openModal()}>Remove a Dinosaur &#129430;</button>
           <Modal visible={this.state.visible} width="55%" height="55%" onClickAway={() => this.closeModal()}>
-            <RemoveDinosaurForm dinosaurList={this.props.dinosaurList} />
+            <RemoveDinosaurForm dinosaurList={this.props.dinosaurList}
+              onRemoveDinoClick={this.props.onRemoveDinoClick}/>
         </Modal>
       </div>
     )
