3fce7b6429d8f8f3978c936120d028d1311377a6
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-21 10:37:42 +0100

    dino container looping through list of dinos, not sorting yet

--- src/containers/DinosaurContainer.js
+++ src/containers/DinosaurContainer.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import DinosaurList from '../components/paddock_dino_container/DinosaurList';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 class DinosaurContainer extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      listOfDinos: []
+      listOfDinos: [],
+      paddockName: "",
+      filteredDinos: []
     }
+    this.filterDinosaurs = this.filterDinosaurs.bind(this)
+  }
+
+  filterDinosaurs() {
+    let dinoArray = this.state.listOfDinos;
+    console.log(dinoArray);
+
+    for (let i = 0; i < dinoArray.length; i++) {
+      let dino = dinoArray[i];
+      console.log(dino);
+    }
+
   }
 
   componentDidMount(){
     const url = 'http://localhost:8080/dinosaurs'
     fetch(url)
     .then(res => res.json())
-    .then(returnedDinos => this.setState({listOfDinos: returnedDinos}))
+    .then(returnedDinos => this.setState({listOfDinos: returnedDinos, paddockName: this.props.match.params.paddock}, () => this.filterDinosaurs() ))
     .catch(err => console.error(err))
   }
 
@@ -24,7 +37,7 @@ class DinosaurContainer extends Component {
       <div className="paddock">
         <DinosaurList listOfDinos={this.state.listOfDinos}/>
         <div className="park-container">
-        <h1>hi</h1> 
+        <h1>hi</h1>
         </div>
       </div>
     );
