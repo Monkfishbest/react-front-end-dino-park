3ea95d9d0b13b8fdf344ba262d225ae2047556cb
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-21 20:54:15 +0100

    Added rampage component

---
+++ src/components/Rampage.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Modal from 'react-awesome-modal';
+import './Rampage.css';
+
+class Rampage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    }
+  }
+
+  openModal() {
+    this.setState({
+        visible : true
+    });
+  }
+
+  closeModal() {
+    this.setState({
+        visible : false
+    });
+    window.location = '/park-map';
+  }
+
+  render() {
+    return (
+      <div className="rampage-div">
+        <button className="rampage-btn" type="button" value="rampage" onClick={() => this.openModal()}>Click to confirm Dinoaurs are going crazy!! &#129430; </button>
+
+        <Modal visible={this.state.visible} width="75%" height="75%" onClickAway={() => this.closeModal()}>
+          <div className="modal-contents">
+            <div className="panic-over-btn-div">
+              <h2>PARK LOCKED DOWN!!</h2>
+              <button className="panic-over-btn" onClick={() => this.closeModal()}>Click to confirm when all the dinosaurs have been secured</button>
+            </div>
+          </div>
+        </Modal>
+
+      </div>
+    )
+  }
+}
+
+export default Rampage;
