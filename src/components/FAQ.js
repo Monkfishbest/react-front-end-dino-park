7771974f56737eee397e1f1f179eca8516b35a6c
Steph Scully <stephanielarascullion@yahoo.com>
2019-08-21 21:49:12 +0100

    FAQ page improved

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -2,20 +2,26 @@ import React from "react";
 import './Faq.css';
 
 const Faq = () => (
-  <div className="about">
+  <div className="faq">
     <h1>
     This is just a page for flavour and doesnt really add anything :)
     </h1>
-    <h4>Q) Why are you building ANOTHER dinosaur themed park? have you not seen what happened the last four times?!</h4>
-    <p>A) We are aware of the concern but with our professional team of devs we are confident that it won't end in mass death!  </p>
-
-    <h4>Q) Why don't the dinosaurs rampage?</h4>
-    <p>A) We keep our dinosaurs seriously drugged to avoid such unfortunate incidents.  </p>
-
-    <h4>Q) How are dinosaurs brought back? what is this magic?! </h4>
-    <p>A) A lab full of highly trained scientests grew them out of a single stock-cube that was once licked by a dog. </p>
-    <h4>Q) What do your dinosaurs look like?!!? </h4>
-    <p>A) Dino park's dinosaurs are best enjoyed through your vivid imagination </p>
+      <div className="q-and-a">
+        <h3>Q) Why are you building ANOTHER dinosaur themed park? have you not seen what happened the last four times?!</h3>
+        <h4>A) We are aware of the concern but with our professional team of devs we are confident that it won't end in mass death!  </h4>
+      </div>
+      <div className="q-and-a">
+      <h3>Q) Why don't the dinosaurs rampage?</h3>
+      <h4>A) We keep our dinosaurs seriously drugged to avoid such unfortunate incidents.  </h4>
+      </div>
+      <div className="q-and-a">
+      <h3>Q) How were dinosaurs brought back? What is this magic?! </h3>
+      <h4>A) A lab full of highly trained scientists grew them out of a single stock-cube that was once licked by a dog. </h4>
+      </div>
+      <div className="q-and-a">
+      <h3>Q) What do your dinosaurs look like?!!? </h3>
+      <h4>A) Dino Park's dinosaurs are best enjoyed through your vivid imagination </h4>
+      </div>
   </div>
 )
 export default Faq;
