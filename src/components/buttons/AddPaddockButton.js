ac3154ac24105d06381065c2eb1b7d8971acfd9f
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-21 14:51:30 +0100

    added more emojis to buttons

--- src/components/buttons/AddPaddockButton.js
+++ src/components/buttons/AddPaddockButton.js
@@ -27,7 +27,7 @@ class AddPaddockButton extends Component {
   render() {
     return (
       <div className="add-paddock-button">
-      <button className="add-paddock-btn" type="button" value="add-paddock" onClick={() => this.openModal()} >Add a Paddock <Emoji symbol="🌳" label="deciduous tree"/></button>
+      <button className="add-paddock-btn" type="button" value="add-paddock" onClick={() => this.openModal()} ><Emoji symbol="🌳" label="deciduous tree"/> Add a Paddock <Emoji symbol="🌳" label="deciduous tree"/></button>
         <Modal visible={this.state.visible} width="55%" height="55%" onClickAway={() => this.closeModal()}>
           <AddPaddockForm onAddPaddockFormSubmit={this.props.onAddPaddockFormSubmit}/>
         </Modal>
