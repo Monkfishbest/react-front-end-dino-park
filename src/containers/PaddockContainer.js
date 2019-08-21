1aa82d42e16ed3800c77200ac0e094f88eb1daad
Robert Tam <rsh.tam@gmail.com>
2019-08-21 14:34:46 +0100

    refactor paddock list in transfer dinos to just herbivore paddocks

--- src/containers/PaddockContainer.js
+++ src/containers/PaddockContainer.js
@@ -9,6 +9,7 @@ class PaddockContainer extends Component {
     super(props);
     this.state = {
       listOfPaddocks: [],
+      listOfHerbivorePaddocks: [],
       listOfHerbivores: [],
       newDino: {},
       listOfDinosaurs: [],
@@ -30,10 +31,14 @@ class PaddockContainer extends Component {
     const dinoRequest = new Request();
     dinoRequest.get("/dinosaurs")
     .then(returnedDinosaurs => this.setState({listOfDinosaurs: returnedDinosaurs}))
+    .catch(err => console.error(err))
 
     request.get("/herbivores")
     .then(herbivores => this.setState({listOfHerbivores: herbivores}))
+    .catch(err => console.error(err))
 
+    request.get("/paddocks/herbivores")
+    .then(returnedPaddocks => this.setState({listOfHerbivorePaddocks: returnedPaddocks}))
     .catch(err => console.error(err))
   }
 
@@ -102,6 +107,7 @@ class PaddockContainer extends Component {
       <div className="PaddockContainer">
         <PaddockList paddockList={this.state.listOfPaddocks}/>
         <ButtonList paddockList={this.state.listOfPaddocks}
+        herbivorePaddockList={this.state.listOfHerbivorePaddocks}
         dinosaurList={this.state.listOfDinosaurs}
         herbivoreList={this.state.listOfHerbivores}
         onAddPaddockFormSubmit={this.handleAddPaddockFormSubmit} onTransferFormSubmit={this.handleTransferFormSubmit} onAddDinosaurFormSubmit={this.handleAddDinosaurFormSubmit}
